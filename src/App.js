2566815b5803c9af3914e62e31cad81abb1e1e7f
Emanuel Molina <128034917+AcquahLopid@users.noreply.github.com>
2023-06-14 05:24:23 -0700

    tried to fix navigation bar issue where the nav bar over laps content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Container from "./components/Container";
-import "./App.css";
 
 const App = () =>
 <Container />;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,37 +7,52 @@ import Emojify from "../assets/Emojify.gif";
 
 function Portfolio() {
     return (
+  <section>
+      <h2 className="section-title secondary-border">Projects</h2>
       <div id="work" className="repository">
-          <div className="flex-row">
-            <h2 className="section-title secondary-border">Projects</h2>
-          </div>
-            <div className="repository">
               <div className="repositoryInfo">
                 <div className="repositoryImg">
                   <a href="https://github.com/samhiga/emojify.git">
                     <img
                       src={Emojify}
-                      className="my-2"
+                      className="my-2 portfolioButton"
+                      href="https://samhiga.github.io/emojify/"
                       style={{ width: "100%" }}
                       alt="project"
                     />
-                    <div className="overlay">
-                      <h4>Emojify</h4>
-                      <p>
-                        With so many movies there is an often struggle to pick one
-                      </p>
-                    </div>
                   </a>
-                </div>
-                <div className="ProjectText">
-                <ul>
-                    <a className="portfolioButton"
-                    href="https://samhiga.github.io/emojify/">Emojify Live Site</a>
-                  </ul>
+                  <a href="https://github.com/samhiga/emojify.git">
+                    <img
+                      src={Emojify}
+                      className="my-2 portfolioButton"
+                      href="https://samhiga.github.io/emojify/"
+                      style={{ width: "100%" }}
+                      alt="project"
+                    />
+                  </a>
+                  <a href="https://github.com/samhiga/emojify.git">
+                    <img
+                      src={Emojify}
+                      className="my-2 portfolioButton"
+                      href="https://samhiga.github.io/emojify/"
+                      style={{ width: "100%" }}
+                      alt="project"
+                    />
+                  </a>
+                  <a href="https://github.com/samhiga/emojify.git">
+                    <img
+                      src={Emojify}
+                      className="my-2 portfolioButton"
+                      href="https://samhiga.github.io/emojify/"
+                      style={{ width: "100%" }}
+                      alt="project"
+                    />
+                  </a>
                 </div>
               </div>
             </div>
-        </div>
+</section>
+
 
     );
   }
